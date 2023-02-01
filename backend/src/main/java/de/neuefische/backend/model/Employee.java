5ca207589202861b24f16a019ab573dadfb523e7
Cengizhan Atakul <cengizatakul@hotmail.de>
2023-02-01 08:58:55 +0100

    add employee model, repo and controller

--- backend/src/main/java/de/neuefische/backend/model/Employee.java
+++ backend/src/main/java/de/neuefische/backend/model/Employee.java
@@ -1,2 +1,23 @@
-package de.neuefische.backend.model;public class Employee {
+package de.neuefische.backend.model;
+
+
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(collection = "employees")
+public class Employee {
+    @Id
+    private String id;
+    private String firstName;
+    private String lastName;
+    private int age;
+    private String position;
+    private String gender;
+    private String company;
+
 }
