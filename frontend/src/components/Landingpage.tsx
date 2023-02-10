06944d8b9f846ad8984744a2b4a9b31e594a8eb3
Cengizhan Atakul <cengizatakul@hotmail.de>
2023-02-10 09:06:40 +0100

    add landigpage

--- frontend/src/components/Landingpage.tsx
+++ frontend/src/components/Landingpage.tsx
@@ -1,9 +1,30 @@
 import React from "react";
+import styled from "styled-components";
+import {Link} from "react-router-dom";
 
 
 export default function Landingpage() {
 
+
     return (
-        <h1>BAUARBEITEN!</h1>
+        <div className="container">
+           <StyledHeader>Welcome to the Employee Management App</StyledHeader>
+            <Link to="/employees">
+                <StyledButton
+                  type="button" className="btn btn-primary btn-lg">Get started
+                </StyledButton>
+            </Link>
+        </div>
     )
 }
+
+const StyledButton = styled.button`
+// text-decoration: none;
+// pointer-events: none;
+`
+
+
+const StyledHeader = styled.h2`
+text-align: center;
+
+`
