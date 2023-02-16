f7505a00be555cf4ff560e9a1b9f89ef54e0e5d7
Cengizhan Atakul <cengizatakul@hotmail.de>
2023-02-16 15:32:58 +0100

    combined EmployeeListElements.tsx and EmployeeSearchBar.tsx into one component. Fixed route in App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import './App.css';
-import EmployeeListElements from "./components/EmployeeListElements";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
@@ -11,44 +10,43 @@ import AddEmployeeForm from "./components/AddEmployeeForm";
 import useEmployees from "./hooks/useEmployees";
 import EditEmployeeForm from "./components/EditEmployeeForm";
 import 'bootstrap/dist/css/bootstrap.min.css'
-
-
+import EmployeeSearchBar from "./components/EmployeeSearchBar";
 
 
 function App() {
-    const { createEmployee, editEmployee, employees } = useEmployees();
+    const {createEmployee, editEmployee, employees, removeEmployee} = useEmployees();
 
     return (
         <>
-            <Header />
+            <Header/>
             <BrowserRouter>
-                <NavigationBar />
+                <NavigationBar/>
                 <Routes>
-                    <Route path={"/"} element={<Landingpage />} />
-                    <Route path={"/employees"} element={<EmployeeListElements />} />
-                    <Route path={"/details"} element={<EmployeeDetails />} />
-                    <Route path={"/add-employee"} element={<AddEmployeeForm createEmployee={createEmployee} />} />
-                    <Route
-                        path={"/edit-employee/:id"}
-                        element={
-                            <EditEmployeeForm
-                                editEmployee={editEmployee}
-                                employees={employees}
-                                employee={{
-                                    id: "",
-                                    name: "",
-                                    position: "",
-                                    gender: "",
-                                    age: "",
-                                    emailId: "",
-                                }}
-                            />
-                        }
+                    <Route path={"/"} element={<Landingpage/>}/>
+                    <Route path={"/employees"} element={<EmployeeSearchBar employees={employees} onSearchChange={() => {
+                    }} removeEmployee={removeEmployee}/>}/>
+                    <Route path={"/details"} element={<EmployeeDetails/>}/>
+                    <Route path={"/add-employee"} element={<AddEmployeeForm createEmployee={createEmployee}/>}/>
+                    <Route path={"/edit-employee/:id"} element={
+                        <EditEmployeeForm
+                            editEmployee={editEmployee}
+                            employees={employees}
+                            employee={{
+                                id: "",
+                                name: "",
+                                position: "",
+                                gender: "",
+                                age: "",
+                                emailId: "",
+                            }}
+                        />
+                    }
                     />
                 </Routes>
             </BrowserRouter>
-            <Footer />
+            <Footer/>
         </>
     );
 }
+
 export default App;
--- frontend/src/components/EmployeeSearchBar.tsx
+++ frontend/src/components/EmployeeSearchBar.tsx
@@ -1,39 +1,63 @@
-import { Employee } from "../model/Employee";
-import { ChangeEvent } from "react";
+import React, {useState} from "react";
+import {Employee} from "../model/Employee";
+import {ChangeEvent} from "react";
 import styled from "styled-components";
+import EmployeeList from "./EmployeeList";
 
 type EmployeeSearchProps = {
     employees: Employee[];
     onSearchChange: (searchTerm: string) => void;
+    removeEmployee: (id: string) => void;
 };
 
-export default function EmployeeSearchBar(props: EmployeeSearchProps) {
-    const { employees, onSearchChange } = props;
+export default function EmployeeSearchBar({
+                                              employees,
+                                              onSearchChange,
+                                              removeEmployee,
+                                          }: EmployeeSearchProps) {
+    const [searchTerm, setSearchTerm] = useState("");
 
     function handleSearchChange(event: ChangeEvent<HTMLInputElement>) {
+        setSearchTerm(event.target.value);
         onSearchChange(event.target.value);
     }
 
+    function filterEmployees() {
+        if (!searchTerm) {
+            return employees;
+        }
+        return employees.filter((employee) =>
+            employee.name.toLowerCase().includes(searchTerm.toLowerCase())
+        );
+    }
+
     return (
-        <div>
-            <StyledInput
-                placeholder={"Search"}
-                onChange={handleSearchChange}
-            />
+        <>
+            <div className="container">
+                <div className="row mb-4">
+                    <div className="col-4">
+                        <StyledInput
+                            type="text"
+                            className="form-control"
+                            placeholder={"Search by name"}
+                            value={searchTerm}
+                            onChange={handleSearchChange}
+                        />
+                    </div>
+                </div>
+            </div>
 
             <StyledDiv>
-                {employees.map((employee) => (
-                    <div key={employee.id}>{employee.name}</div>
-                ))}
+                <EmployeeList employees={filterEmployees()} removeEmployee={removeEmployee}/>
             </StyledDiv>
-        </div>
+        </>
     );
 }
 
 const StyledInput = styled.input`
   margin-bottom: 10px;
   padding: 5px;
-  width: 100%;
+  width: 50%;
 `;
 
 const StyledDiv = styled.div`
